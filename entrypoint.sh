55996d4bb02c61fbfb00d9394f40a655eb8cb0ca
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-10-15 19:57:08 +0200

    correct local IP config

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ APIBAN_KEY="${APIBAN_KEY:-0000000000000000000000}"
 
 sed -i "s/HOMER_IP_HERE/$HOMER_IP/g" /app/config.js
 sed -i "s/HOMER_PORT_HERE/$HOMER_PORT/g" /app/config.js
-sed -i "s/PUBLIC_IP_HERE/$PUBLIC_IP/g" /app/config.js
+sed -i "s/LOCAL_IP_HERE/$PUBLIC_IP/g" /app/config.js
 sed -i "s/APIBAN_KEY_HERE/$APIBAN_KEY/g" /app/config.js
 
 cat /app/config.js
